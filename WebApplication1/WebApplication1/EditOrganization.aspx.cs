342ede24a347a621f7e8ec03849f7f9420ab9bfb
--global <tandoni@TANDONI-W530G2.rose-hulman.edu>
2015-05-21 21:08:33 -0400

    no message

--- WebApplication1/WebApplication1/EditOrganization.aspx.cs
+++ WebApplication1/WebApplication1/EditOrganization.aspx.cs
@@ -96,7 +96,7 @@ namespace WebApplication1
                 cmd2.Parameters.Add("@description", SqlDbType.VarChar).Value = editedDesc;
                 cmd2.Parameters.Add("@lid", SqlDbType.Int).Value = Convert.ToInt32(dropDownLocations.SelectedItem.Value);
                 cmd2.Parameters.Add("@name2", SqlDbType.VarChar).Value = dropDownPartOfOther.SelectedItem.Value.ToString();
-                con.Open();
+                con.Open(); 
                 cmd2.ExecuteNonQuery();
                 con.Close();
 
